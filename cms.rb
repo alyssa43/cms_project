ec7e659c2316aa131c640bf715c04d559822f403
Alyssa Easter <AlyssaStarley@gmail.com>
2025-02-11 17:06:02 -0800

    Completed assignment 4 - Viewing Text Files

--- cms.rb
+++ cms.rb
@@ -4,8 +4,20 @@ require "tilt/erubi"
 
 DATA_PATH = File.join(__dir__, "data") # returns String path to `data` directory => "/Users/alyssaeaster/cms_project/data"
 
+def get_file_path(file_name)
+  File.join(__dir__, "data", file_name)
+end
+
 get "/" do
-  @files = Dir.glob(File.join(DATA_PATH, "*")) # returns an Array of string File paths => ["/Users/alyssaeaster/cms_project/data/about.txt", "/Users/alyssaeaster/cms_project/data/changes.txt", "/Users/alyssaeaster/cms_project/data/history.txt"]
+  @files = Dir.glob(File.join(DATA_PATH, "*")).map do |path|
+    File.basename(path)
+  end # returns an Array of string File paths => ["about.txt", "changes.txt", "history.txt"]
 
   erb :index
+end
+
+get "/:file" do
+  file_path = get_file_path(params[:file])
+  headers["Content-Type"]= "text/plain"
+  File.read(file_path)
 end
