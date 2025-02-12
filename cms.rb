4b26eba9cd994690a7ca2eab56c292fc4fb0a3dc
Alyssa Easter <AlyssaStarley@gmail.com>
2025-02-11 16:23:32 -0800

    Completed assignment 3 - Adding an Index Page

--- cms.rb
+++ cms.rb
@@ -1,6 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubi"
+
+DATA_PATH = File.join(__dir__, "data") # returns String path to `data` directory => "/Users/alyssaeaster/cms_project/data"
 
 get "/" do
-  "Getting started."
+  @files = Dir.glob(File.join(DATA_PATH, "*")) # returns an Array of string File paths => ["/Users/alyssaeaster/cms_project/data/about.txt", "/Users/alyssaeaster/cms_project/data/changes.txt", "/Users/alyssaeaster/cms_project/data/history.txt"]
+
+  erb :index
 end
